8c6e7dd32a7a6c378988705d1e211b34fcce6316
Andrés Howard <ahowardm@gmail.com>
2025-04-22 09:54:40 -0400

    add callbacks and validations to models

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  before_validation :normalize_name
+
+  private
+  def normalize_name
+    self.first_name = first_name.strip().titleize()
+    self.last_name = last_name.strip.titleize
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :publication_date, presence: true
+
+  before_validation :normalize_title
+
+  private
+  def normalize_title
+    self.title = title.strip().titleize()
+  end
 end
